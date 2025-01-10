13b998081ab24b3e61cd9232d6176512dd0c0e1d
Faizan Yousafzai <97096722+Faizan301@users.noreply.github.com>
2025-01-10 07:36:05 +0000

    variable file added

---
+++ 01_basic/variable.js
@@ -0,0 +1,6 @@
+const accountId = 101;
+let accountName = "Ali";
+var accountEmail = "ali@gmail.com";
+accountPassword = '12345';
+
+console.table([accountId,accountName,accountEmail,accountPassword]);
